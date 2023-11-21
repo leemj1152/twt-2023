1e2c412d0ebadc35ba325c657d11b18708c1d2cf
leemj1152 <79883255+leemj1152@users.noreply.github.com>
2023-11-21 23:08:34 +0900

    form / tweet 컴포넌트

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -60,6 +63,8 @@ const SubmitBtn = styled.input`
   }
 `;
 
+const FILE_SIZE_MAX_LIMIT = 1 * 1024 * 1024; //1gb
+
 export default function PostTweetForm() {
   const [isLoading, setLoading] = useState(false);
   const [tweet, setTweet] = useState("");
@@ -69,13 +74,46 @@ export default function PostTweetForm() {
   };
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
-    if (files && files.length === 1) {
+    if (files && files.length === 1 && files[0].size > FILE_SIZE_MAX_LIMIT) {
       setFile(files[0]);
     }
   };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+
+    try {
+      setLoading(true);
+      const doc = await addDoc(collection(db, "tweets"), {
+        tweet,
+        createAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
+      setTweet("");
+      setFile(null);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={200}
         onChange={onChange}
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDCqQCrPT7C6tecQ-GyDfzyvobc0r94qc",
@@ -7,8 +9,13 @@ const firebaseConfig = {
   projectId: "twt-2023",
   storageBucket: "twt-2023.appspot.com",
   messagingSenderId: "691993231973",
-  appId: "1:691993231973:web:49c405d8abeff74509bc8c"
+  appId: "1:691993231973:web:49c405d8abeff74509bc8c",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
